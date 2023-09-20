c2282771523affbb30c788e9f4bf9a91dad67b67
ccassani <66717275+ccassani@users.noreply.github.com>
2023-09-20 14:30:21 +0000

    New color 5

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const getColor2 = function () {
 };
 
 const getColor3 = function () {
-  const color = [0, 255, 0];
+  const color = [240,128,128];
   return color;
 };
 
@@ -23,7 +23,7 @@ const getColor4 = function () {
 };
 
 const getColor5 = function () {
-  const color = [25, 0, 255];
+  const color = [168, 80, 50];
   return color;
 };
 
